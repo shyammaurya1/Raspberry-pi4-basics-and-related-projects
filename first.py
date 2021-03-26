77e94af816196c10d4eeddab58c50acd4ae949ac
shyam maurya <shyammaurya395@gmail.com>
2021-03-26 21:06:08 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,16 +1,18 @@
+#program to light bulb(led)
+
 import RPi.GPIO as GPIO
 import time
 
-LED_PIN = 17
+LED_PIN = 17 #check GPIO pin no.
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.setup(LED_PIN, GPIO.OUT) #output
 
 while True:
-    GPIO.output(LED_PIN, GPIO.HIGH)
+    GPIO.output(LED_PIN, GPIO.HIGH) #HIGH : ON
     time.sleep(1)
-    GPIO.output(LED_PIN, GPIO.LOW)
+    GPIO.output(LED_PIN, GPIO.LOW) #LOW :OFF
     time.sleep(1)
 
 GPIO.cleanup()
